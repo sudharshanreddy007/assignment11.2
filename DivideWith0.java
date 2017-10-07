03489162f4b80db2c2abde362481f5cfe11793bb
sudharshanreddy007 <31057445+sudharshanreddy007@users.noreply.github.com>
2017-10-07 13:42:49 +0530

    Add files via upload

---
+++ DivideWith0.java
@@ -0,0 +1,14 @@
+package divide;
+
+public class DivideWith0 {
+
+public static void main(String []args){//intializing 3 integer variables and their values
+		
+		int i ,j ,k ;
+		 i= 8;
+		 j = 0 ;
+		 k= i/ j;
+		 System.out.println(k);
+	}
+
+}
